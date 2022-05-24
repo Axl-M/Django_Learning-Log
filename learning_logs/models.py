ea7d86b39c7c18b4b7856468797bbc33de449fca
Alexander M <axlmalyasov@gmail.com>
2022-05-24 12:04:16 +1000

    model Entry created

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -3,9 +3,21 @@ from django.db import models
 # Create your models here.
 class Topic(models.Model):
     """Тема, которую изучает пользователь"""
+    # то есть заголовок
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         """Возвращает строковое представление модели."""
         return self.text
 
+class Entry(models.Model):
+    """Информация, изученная пользователем по теме"""
+    # текст который будет ассоциирован с одной из тем
+    topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = 'entries'
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        return self.text[:50] + "..."
