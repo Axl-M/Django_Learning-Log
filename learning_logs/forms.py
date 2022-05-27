45bb3e5d2b0415eb46c9f6560b9d2bfcbc80e09f
Alexander M <axlmalyasov@gmail.com>
2022-05-27 22:08:57 +1000

    now user can add new topic

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Topic
 
-classTopicForm(forms.ModelForm):
+
+class TopicForm(forms.ModelForm):
     class Meta:
-        model = Topic
-        fields = ['text']
-        labels = {'text': ''}
+        # класс Meta сообщает Django, на какой модели должна базироваться форма и какие поля на ней должны находиться.
+        model = Topic               # форма создается на базе модели Topic
+        fields = ['text']           # на ней размещается только поле text
+        labels = {'text': ''}       # не генерировать подпись для текстового поля
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+# from django.core.urlresolvers import reverse
+from django.urls import reverse
 from .models import Topic
+from .forms import TopicForm
+
 
 # Create your views here.
 def index(request):
     """Домашняя страница приложения Learning Log"""
     return render(request, 'learning_logs/index.html')
+
 
 def topics(request):
     """ Выводит список тем """
@@ -19,3 +25,24 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Определяет новую тему."""
+    if request.method != 'POST':    # запросил ли пользователь пустую форму (запрос GET)
+        # Данные не отправлялись; создается пустая форма.
+        form = TopicForm()
+    else:
+        # Обработать заполненную форму (запрос POST).
+        # Отправлены данные POST; обработать данные.
+        form = TopicForm(request.POST)
+        # Отправленную информацию нельзя сохранять в базе данных пока она не будет проверена
+        if form.is_valid():
+            form.save()
+            # Перенаправление пользователя к странице topics после отправки введенной темы
+        # Функция reverse() определяет URL по заданной схеме URL(то есть Django сгенерирует URL при запросе страницы)
+            # На этой странице пользователь видит только что введенную им тему в общем списке тем.
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+# Так как при создании TopicForm аргументы не передавались, Django создает пустую форму, которая заполняется пользователем.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
