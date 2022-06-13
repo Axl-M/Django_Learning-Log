2a17b8b1686ab13e09e483a01e3f424603727ee5
Alexander M <axlmalyasov@gmail.com>
2022-06-13 18:50:09 +1000

    Linked Topic with User

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Topic(models.Model):
@@ -6,18 +8,23 @@ class Topic(models.Model):
     # то есть заголовок
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)     # связываем ТЕМУ и ПОЛЬЗОВАТЕЛЯ
+
     def __str__(self):
         """Возвращает строковое представление модели."""
         return self.text
 
+
 class Entry(models.Model):
     """Информация, изученная пользователем по теме"""
-    # текст который будет ассоциирован с одной из тем
+    # текст, который будет ассоциирован с одной из тем
     topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         verbose_name_plural = 'entries'
+
     def __str__(self):
         """Возвращает строковое представление модели."""
         if len(self.text) < 50:
